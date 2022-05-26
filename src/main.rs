734a5ab36e656a900e3b18f9a89b4b0c44970feb
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-05-26 13:46:13 -0500

    Test with example pest file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod ast;
+mod parser;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "example.pest";
+    let file = std::fs::read_to_string(filename).expect(filename);
+    let response = parser::parse(&file, false);
+    if let Err(error) = response {
+        println!("{}", error);
+        std::process::exit(1);
+    }
+    let ast = response.unwrap();
+    println!("{ast:#?}");
 }
